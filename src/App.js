b4de07d65121b79e82b6a5badaf798cfdac4932d
Cami <camibami7@gmail.com>
2021-08-12 11:42:33 -0600

    update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import { GetRequest } from './GetRequest';
 const App = () => {
   return (
     <div class="page_container">
-      <div id="content-wrap">
       <h1 class="ms_header">movie search</h1>
       <GetRequest />
-      </div>
       <div class="footer"><footer>© Cami Godfrey 2021 | All Rights Reserved</footer></div>
     </div>
   );
